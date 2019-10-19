6a120df49d5eaf3711c3f396f3fccec9ddd70125
Zbigniew Heintze <zheintze@gmail.com>
2019-10-19 12:33:49 +0200

    Implemented regonapi library

---
+++ regonapi/api.py
@@ -0,0 +1,138 @@
+import logging
+import re
+from typing import Any, Dict, List, Optional
+
+from lxml import objectify
+from lxml.objectify import ObjectifiedElement
+
+from .client import Client
+
+logger = logging.getLogger(__name__)
+
+
+class RegonAPI:
+    def __init__(self, api_key: Optional[str] = None):
+        self._client = Client(api_key=api_key)
+
+    def _format(self, result: ObjectifiedElement, remove_prefix: bool = False) -> Dict[str, Any]:
+        formatted_result = {}
+        for field in result.getchildren():
+            name = field.tag
+            if remove_prefix:
+                name = self._remove_prefix(name)
+            name = self._underscore(name)
+            formatted_result[name] = field.text
+        return formatted_result
+
+    def _normalize(self, result: Dict[str, Any], field_map: List) -> Dict[str, Any]:
+        normalized_result = {}
+        for field, alias, callback in field_map:
+            if field not in result:
+                logger.warning(f"Missing field {field} in result {result}")
+                value = None
+            else:
+                value = result[field]
+
+            if value is not None and callback:
+                value = callback(value)
+
+            normalized_result[alias or field] = value
+        return normalized_result
+
+    @staticmethod
+    def _remove_prefix(name: str) -> str:
+        return name.split("_", 1)[1]
+
+    @staticmethod
+    def _underscore(word: str) -> str:
+        """
+        Make an underscored, lowercase form from the expression in the string.
+
+        Instead of this method you can use 'underscore' from the inflection library,
+        but I didn't want to add a new requirement
+        """
+        word = re.sub(r"([A-Z]+)([A-Z][a-z])", r"\1_\2", word)
+        word = re.sub(r"([a-z\d])([A-Z])", r"\1_\2", word)
+        word = word.replace("-", "_")
+        return word.lower()
+
+    def find_by(self, *, nip: str = None, regon: str = None, krs: str = None) -> List[Dict[str, Any]]:
+        field_map = [
+            ("data_zakonczenia_dzialalnosci", None, None),
+            ("gmina", None, None),
+            ("kod_pocztowy", None, None),
+            ("miejscowosc", None, None),
+            ("nazwa", None, None),
+            ("nip", None, None),
+            ("nr_lokalu", None, None),
+            ("nr_nieruchomosci", None, None),
+            ("powiat", None, None),
+            ("regon", None, None),
+            ("silos_id", None, None),
+            ("status_nip", None, None),
+            ("typ", None, None),
+            ("ulica", None, None),
+            ("wojewodztwo", None, None),
+        ]
+        result = self._client.search(nip=nip, regon=regon, krs=krs)
+        # In this case the normalization was added only because the API sandbox returns a different data structure
+        # for endpoint 'DaneSzukajPodmioty' than the production version. The 'Ulica' field is missing.
+        return [self._normalize(self._format(item), field_map) for item in objectify.fromstring(result).dane]
+
+    def get_full_report(self, company_data: Dict[str, Any]) -> List[Dict[str, Any]]:
+        try:
+            result = self._client.get_full_report(
+                regon=company_data["regon"], company_type=company_data["typ"], silos_id=company_data["silos_id"]
+            )
+        except KeyError:
+            raise ValueError("The company_data parameter should be single item of result of method 'find_by'")
+        return [self._format(item, remove_prefix=True) for item in objectify.fromstring(result).dane]
+
+    def get_pkd(self, company_data: Dict[str, Any]) -> List[Dict[str, Any]]:
+        field_map = [
+            ("pkd_kod", "kod", str),
+            ("pkd_nazwa", "nazwa", str),
+            ("pkd_przewazajace", "przewazajace", lambda v: bool(int(v))),
+        ]
+        try:
+            result = self._client.get_pkd_raport(regon=company_data["regon"], company_type=company_data["typ"])
+        except KeyError:
+            raise ValueError("The company_data parameter should be single item of result of method 'find_by'")
+
+        return [
+            self._normalize(self._format(item, remove_prefix=True), field_map=field_map)
+            for item in objectify.fromstring(result).dane
+        ]
+
+    def get_address(self, company_data: Dict[str, Any]) -> Dict[str, Any]:
+        try:
+            address = f"{company_data['ulica']} {company_data['nr_nieruchomosci']}"
+            if company_data["nr_lokalu"]:
+                address += f"/{company_data['nr_lokalu']}"
+
+            return {
+                "ulica": company_data["ulica"],
+                "nr_nieruchomosci": company_data["nr_nieruchomosci"],
+                "nr_lokalu": company_data["nr_lokalu"],
+                "adres": address,
+                "kod_pocztowy": company_data["kod_pocztowy"],
+                "miejscowosc": company_data["miejscowosc"],
+                "gmina": company_data["gmina"],
+                "powiat": company_data["powiat"],
+                "wojewodztwo": company_data["wojewodztwo"],
+            }
+        except KeyError:
+            raise ValueError("The company_data parameter should be single item of result of method 'find_by'")
+
+    def get_contact(self, full_report_data: Dict[str, Any]) -> Dict[str, Any]:
+        if not ("regon9" in full_report_data or "regon14" in full_report_data):
+            raise ValueError(
+                "The full_report_data parameter should be single item of result of method 'get_full_report'"
+            )
+        return {
+            "nr_telefonu": full_report_data.get("numer_telefonu"),
+            "nr_wewnetrzny_telefonu": full_report_data.get("numer_wewnetrzny_telefonu"),
+            "nr_faksu": full_report_data.get("numer_faksu"),
+            "email": full_report_data.get("adres_email"),
+            "www": full_report_data.get("adres_stronyinternetowej"),
+        }
---
+++ regonapi/client.py
@@ -0,0 +1,110 @@
+import logging
+from typing import Optional
+
+from lxml import objectify
+from lxml.etree import XMLSyntaxError
+from requests import Session
+from zeep import Client as ZeepClient
+from zeep import Transport
+
+logger = logging.getLogger(__name__)
+
+
+WSDL = "https://wyszukiwarkaregon.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-prod.wsdl"
+ENDPOINT = "https://wyszukiwarkaregon.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc"
+
+WSDL_SANDBOX = "https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-test.wsdl"
+ENDPOINT_SANDBOX = "https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc"
+APIKEY_SANDBOX = "abcde12345abcde12345"
+
+
+DETAILED_REPORT_NAMES_BY_TYPE = {
+    # podmiot osoby fizycznej prowadzącej działalność gospodarczą
+    "F": {
+        # działalność podlegającą wpisowi do CEIDG
+        "1": "BIR11OsFizycznaDzialalnoscCeidg",
+        # działalność rolnicza (gospodarstwo rolne, działy specjaln eprodukcji rolnej)
+        "2": "BIR11OsFizycznaDzialalnoscRolnicza",
+        # działalność inna (komornik, notariusz, agroturystyka – o ile nie wpisane do CEIDG)
+        "3": "BIR11OsFizycznaDzialalnoscPozostala",
+        # działalność skreślona z rejestru REGON przed datą 08.11.2014 (w poprzednim systemie inform.)
+        "4": "BIR11OsFizycznaDzialalnoscSkreslonaDo20141108",
+    },
+    # jednostka lokalna podmiotu osoby fizycznej
+    "LF": "BIR11JednLokalnaOsFizycznej",
+    # osoba prawna oraz jednostka organizacyjna niemająca osobowości prawnej
+    # ma przypisaną wartość SilosID równą 6 - w dokumentacji opisana jako - wartość techniczna,
+    # bez znaczenia merytorycznego; istnieje wyłącznie dla zachowania spójności struktury
+    "P": "BIR11OsPrawna",
+    # jednostka lokalna osoby prawnej
+    "LP": "BIR11JednLokalnaOsPrawnej",
+}
+
+PKD_REPORT_TYPE = {"F": "PublDaneRaportDzialalnosciFizycznej", "P": "PublDaneRaportDzialalnosciPrawnej"}
+
+
+class Client(object):
+
+    headers = {}
+
+    def __init__(self, api_key: Optional[str] = None):
+
+        wsdl = WSDL if api_key else WSDL_SANDBOX
+        self.endpoint = ENDPOINT if api_key else ENDPOINT_SANDBOX
+        self.api_key = api_key or APIKEY_SANDBOX
+
+        transport = Transport(session=Session())
+        transport.session.headers = self.headers
+        self.client = ZeepClient(wsdl, transport=transport)
+        self.service = None
+
+    def _call(self, action: str, *args, **kwargs) -> str:
+        if not self.service:
+            self.service = self.client.create_service("{http://tempuri.org/}e3", self.endpoint)
+            self.headers.update({"sid": self._login()})
+
+        service = getattr(self.service, action)
+        return service(*args, **kwargs)
+
+    def _login(self) -> str:
+        return self._call("Zaloguj", self.api_key)
+
+    def search(self, *, nip: Optional[str] = None, regon: Optional[str] = None, krs: Optional[str] = None) -> str:
+        if not any([nip, regon, krs]):
+            raise AttributeError("At least one parameter (nip, regon, krs) is required.")
+        if nip:
+            search_params = {"Nip": nip}
+        elif regon:
+            search_params = {"Regon": regon}
+        else:
+            search_params = {"Krs": krs}
+        return self.validate_result(self._call("DaneSzukajPodmioty", search_params))
+
+    def get_full_report(self, regon: str, company_type: str, silos_id: Optional[int] = None) -> str:
+        report_name = DETAILED_REPORT_NAMES_BY_TYPE.get(company_type)
+        if isinstance(report_name, dict):
+            report_name = report_name[silos_id]
+        return self.validate_result(self._call("DanePobierzPelnyRaport", regon, report_name))
+
+    def get_pkd_raport(self, regon: str, company_type: str) -> str:
+        report_type = PKD_REPORT_TYPE[company_type] if company_type in PKD_REPORT_TYPE else PKD_REPORT_TYPE["F"]
+
+        return self.validate_result(self._call("DanePobierzPelnyRaport", regon, report_type))
+
+    @staticmethod
+    def validate_result(result: str) -> str:
+        if not result:
+            raise ValueError(f"Empty result - probably problem with authorisation")
+
+        try:
+            result_object = objectify.fromstring(result)
+        except XMLSyntaxError:
+            raise ValueError(f"Unexpected response: {result}")
+
+        if not hasattr(result_object, "dane"):
+            raise ValueError(f"Unexpected response: {result}")
+
+        if hasattr(result_object.dane, "ErrorCode"):
+            raise ValueError(f"code: {result_object.dane.ErrorCode} - {result_object.dane.ErrorMessageEn}")
+
+        return result
